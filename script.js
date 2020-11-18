4e7487a3694aea9160f4e1121d61f8235f02fb17
alexpryp <alexpryp@gmail.com>
2020-11-18 22:06:57 +0200

    Working with forms in the Vue.js

---
+++ script.js
@@ -0,0 +1,27 @@
+var app1 = new Vue({
+    el: '#app1',
+    data: {
+        message: '',
+    }
+});
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: ''
+    }
+});
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        checked: true,
+    }
+});
+
+var app4 = new Vue({
+    el: '#app4',
+    data: {
+        checkedNames: ["Джон"],
+    },
+});
