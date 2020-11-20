4cbe16254731d9e895bf53044dba1dcaa4b5b499
alexpryp <alexpryp@gmail.com>
2020-11-20 16:26:39 +0200

    Added some more forms

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ var app4 = new Vue({
         checkedNames: ["Джон"],
     },
 });
+
+var app5 = new Vue ({
+    el: '#app5',
+    data: {
+        picked: "Два",
+    },
+});
+
+var app6 = new Vue ({
+    el: '#app6',
+    data: {
+        selected: "",
+    },
+});
+
+var app7 = new Vue ({
+    el: '#app7',
+    data: {
+        selected: "",
+    },
+});
+
+var app8 = new Vue ({
+    el: '#app8',
+    data: {
+        selected: 'A',
+        options: [
+            { text: 'Один', value: 'А' },
+            { text: 'Два', value: 'Б' },
+            { text: 'Три', value: 'В' },
+        ]
+    }
+});
